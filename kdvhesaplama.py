7260f7bbad76db2af0969a890f6cdfa796aa1fe3
Muhammed Doğrudağ <52139039+muhammeddgrdg@users.noreply.github.com>
2021-03-08 02:00:33 +0300

    Update kdvhesaplama.py

--- kdvhesaplama.py
+++ kdvhesaplama.py
@@ -1,7 +1,7 @@
 a = int(input("Fiyat:  "))
 b = float(input("KDV Oranı: "))
 
-print("Fiyat: ",a)
-print("KDV Oranı (%):",b)
-print("KDV Fiyatı: ",a*b/100)
-print("KDV Dahil Toplam Fiyat:", a + a*b/100)
+print("Fiyat: ",a,"₺")
+print("KDV Oranı: ","%",b)
+print("KDV Fiyatı: ",a*b/100,"₺")
+print("KDV Dahil Toplam Fiyat:", a + a*b/100, "₺")
